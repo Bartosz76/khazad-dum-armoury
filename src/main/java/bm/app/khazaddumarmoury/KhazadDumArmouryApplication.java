f2a6750b27fb27c8752e1bfe0ddb15aa4680fd7d
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-06-04 18:11:17 +0200

    The initial commit.

---
+++ src/main/java/bm/app/khazaddumarmoury/KhazadDumArmouryApplication.java
@@ -0,0 +1,13 @@
+package bm.app.khazaddumarmoury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhazadDumArmouryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhazadDumArmouryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bm/app/khazaddumarmoury/KhazadDumArmouryApplicationTests.java
@@ -0,0 +1,13 @@
+package bm.app.khazaddumarmoury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhazadDumArmouryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
