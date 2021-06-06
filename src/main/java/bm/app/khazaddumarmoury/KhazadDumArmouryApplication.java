4fa88774b0aa378fc239cfa98ac638c9a141de5e
Dazikiri Anikar <mierzwinski.b@gmail.com>
2021-06-07 00:55:40 +0200

    Tested the usage of the third method of solving the conflict of two implementations of an interface being available with setting a Bean method in the main class.

--- src/main/java/bm/app/khazaddumarmoury/KhazadDumArmouryApplication.java
+++ src/main/java/bm/app/khazaddumarmoury/KhazadDumArmouryApplication.java
@@ -1,8 +1,10 @@
 package bm.app.khazaddumarmoury;
 
+import bm.app.khazaddumarmoury.armour.domain.ArmourService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KhazadDumArmouryApplication {
@@ -11,5 +13,14 @@ public class KhazadDumArmouryApplication {
 		SpringApplication.run(KhazadDumArmouryApplication.class, args);
 	}
 
+	// The below method is yet another variant of solving the conflict of two available
+	// implementations of the repository interface that is being called by the service.
+	// Instead of making both of those implementations beans, I can just leave them be,
+	// and specify a @Bean method here, returning the repository of my choosing out of
+	// the two.
+//	@Bean
+//	ArmourService armourService() {
+//		return new TardhagharArmourRepository();
+//	}
 
 }
